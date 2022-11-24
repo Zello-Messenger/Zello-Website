9632589fd2ed0a0da9913cd4d1416911b6b9c947
Gustavo Tatarem <31068254+gustatarem@users.noreply.github.com>
2022-11-24 11:10:35 -0300

    Finished mobile layout v1

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,15 +1,106 @@
 import React from "react";
 
-import { Container, Header, HeaderContent } from './styles';
+import { 
+  Container,
+  Header,
+  HeaderContent,
+  Content,
+  IntroSection,
+  FeatureSection,
+  Feature,
+  EmailSection,
+  TestimonySection,
+  PrivacySection,
+  CommunitySection,
+  CommunityFeature,
+  AppSection,
+  Separator,
+  Footer,
+  FooterSocialSection
+} from './styles';
+
 import logoImage from '../../assets/header_logo.svg';
+import mockupImage from '../../assets/zello_mockup.svg';
+import messageIcon from '../../assets/message_icon.svg';
+import clockIcon from '../../assets/clock_icon.svg';
+import priorityIcon from '../../assets/priority_icon.svg';
+import testimonyImage from '../../assets/testimony_image.svg';
+import testimonyMessage from '../../assets/testimony_message.svg';
+import mediumLogo from '../../assets/zello_medium_logo.svg';
+import linkIcon from '../../assets/link_icon.svg';
+import qrCodeIcon from '../../assets/qrcode_icon.svg';
+import carouselImage1 from '../../assets/carousel_image_1.svg';
+import footerLogo from '../../assets/zello_footer_logo.svg';
+import instagramLogo from '../../assets/instagram_logo.svg';
+import appStoreLogo from '../../assets/app_store_logo.svg';
 
 const Home: React.FC = () => (
   <Container>
     <Header>
       <HeaderContent>
-        <img src={logoImage} alt="Zello Messenger"/>
+        <a href="/"><img src={logoImage} alt="Zello Messenger"/></a>
       </HeaderContent>
     </Header>
+    <Content>
+      <IntroSection>
+        <h1>O chat seguro que conecta profissionais da saúde a seus pacientes</h1>
+        <img src={mockupImage} alt="Zello Messenger"/>
+      </IntroSection>
+      <FeatureSection>
+        <Feature>
+          <img src={messageIcon} alt="Zello Messenger"/>
+          <p>Armazene trocas de mensagens com seus pacientes de forma segura</p>
+        </Feature>
+        <Feature>
+          <img src={clockIcon} alt="Zello Messenger"/>
+          <p>Descansar é importante! Receba notificações somente no horário de trabalho.</p>
+        </Feature>
+        <Feature>
+          <img src={priorityIcon} alt="Zello Messenger"/>
+          <p>Seja notificado quando algum de seus pacientes prioritários entrar em contato</p>
+        </Feature>
+      </FeatureSection>
+      <EmailSection>
+        <h1>Seja um dos primeiros a experimentar a segurança, exclusividade e praticidade do Zello</h1>
+        <input type="text" />
+      </EmailSection>
+      <TestimonySection>
+        <img src={testimonyImage} alt="Zello Messenger"/>
+        <p>“Contar com um aplicativo de mensagens direcionado a assuntos profissionais, em separado das conversas com família e amigos, é ótimo para a organização no dia a dia do médico”</p>
+        <img src={testimonyMessage} alt="Zello Messenger" />
+      </TestimonySection>
+      <PrivacySection>
+        <img src={mediumLogo} alt="Zello Messenger" />
+        <h1>Armazene trocas de mensagens com seus pacientes de forma segura</h1>
+        <h2>Sua privacidade é respeitada</h2>
+        <p>O fornecimento de dados pessoais nunca é obrigatório. Você sempre pode decidir se deseja ou não fornecer dados pessoais. <br /><br />Quando solicitarmos a entrada de dados pessoais, indicaremos quais dados são necessários para fazer uso do serviço e, portanto, devem ser fornecidos, e quais dados podem ser fornecidos opcionalmente.</p>
+      </PrivacySection>
+      <CommunitySection>
+        <h1>Comunidade Zello</h1>
+        <h3>Junte-se ao corpo de profissionais da saúde que transformam sua comunicação</h3>
+        <h2>Inicie chats com quem desejar</h2>
+        <CommunityFeature>
+          <img src={linkIcon} alt="Zello Messenger" />
+          <p>Envie um link de convite</p>
+        </CommunityFeature>
+        <CommunityFeature>
+          <img src={qrCodeIcon} alt="Zello Messenger" />
+          <p>Mostre seu QR Code</p>
+        </CommunityFeature>
+      </CommunitySection>
+      <AppSection>
+        <h1>O app</h1>
+        <img src={carouselImage1} alt="Zello Messenger" />
+      </AppSection>
+      <Separator />
+      <Footer>
+        <img src={footerLogo} alt="" />
+        <FooterSocialSection>
+          <a href="https://www.instagram.com/zellomessenger/"><img src={instagramLogo} alt="Zello Messenger" /></a>
+          <img src={appStoreLogo} alt="Zello Messenger" />
+        </FooterSocialSection>
+      </Footer>
+    </Content>
   </Container>
 );
 
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -3,8 +3,9 @@ import styled from 'styled-components';
 export const Container = styled.div``;
 
 export const Header = styled.div`
+  width: 100%;
   padding: 30px 30px;
-  background: #fff;
+  background: #F1F5F7;
   position: fixed;
 `;
 
@@ -13,7 +14,223 @@ export const HeaderContent = styled.div`
   margin: 0 auto;
   display: flex;
   align-items: flex-start;
-  > img {
-    height: 40px;
-  }
 `;
+
+export const Content = styled.div`
+  margin: auto;
+  padding: 120px 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+
+export const IntroSection = styled.div`
+  h1 {
+    display: flex;
+    font-size: 22px;
+    text-align: center;
+    margin: 0 70px;
+    background: -webkit-linear-gradient(#2ABEBE, #08A5F1);
+    background-clip: text;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
+
+  img {
+    display: flex;
+    margin: 15px auto;
+  }
+`
+
+export const FeatureSection = styled.div`
+  margin-top: 40px;
+`
+
+export const Feature = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin: 40px 35px;
+
+  p {
+    font-weight: lighter;
+    margin-left: 24px;
+  }
+`
+
+export const EmailSection = styled.div`
+  background-color: #fff;
+  margin: 0 auto;
+  text-align: center;
+  width: 100%;
+  
+  h1 {
+    margin: 30px;
+    font-weight: bold;
+    font-size: 15px;
+    text-align: center;
+  }
+
+  input {
+    margin-bottom: 30px;
+  }
+`
+
+export const TestimonySection = styled.div`
+  background-color: #DDE8F0;
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  padding: 35px;
+
+  p {
+    text-align: center;
+    font-weight: lighter;
+    font-style: italic;
+    font-size: 14px;
+    margin: 20px 0;
+  }
+`
+
+export const PrivacySection = styled.div`
+  padding: 35px;
+  display: flex;
+  flex-direction: column;
+
+  img {
+    height: 70px;
+  }
+
+  h1 {
+    display: flex;
+    font-size: 22px;
+    text-align: center;
+    margin: 24px 35px;
+    background: -webkit-linear-gradient(#2ABEBE, #08A5F1);
+    background-clip: text;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
+
+  h2 {
+    font-size: 15px;
+    margin-bottom: 10px;
+    background: -webkit-linear-gradient(#2ABEBE, #08A5F1);
+    background-clip: text;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
+
+  p {
+    font-weight: lighter;
+    font-size: 14px;
+  }
+`
+
+export const CommunitySection = styled.div`
+  background-color: #fff;
+  text-align: center;
+  width: 100%;
+  padding: 35px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  h1 {
+    display: flex;
+    font-size: 22px;
+    text-align: center;
+    background: -webkit-linear-gradient(#2ABEBE, #08A5F1);
+    background-clip: text;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
+
+  h2 {
+    font-size: 15px;
+    background: -webkit-linear-gradient(#2ABEBE, #08A5F1);
+    background-clip: text;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
+
+  h3 {
+    font-size: 16px;
+    font-weight: 500;
+    margin-top: 24px;
+    margin-bottom: 35px;
+  }
+`
+
+export const CommunityFeature = styled.div`
+  margin-top: 20px;
+
+  img {
+    height: 70px;
+  }
+
+  p {
+    font-weight: lighter;
+    font-size: 14px;
+  }
+`
+
+export const AppSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 35px;
+  margin-bottom: 10px;
+
+  h1 {
+    display: flex;
+    font-size: 22px;
+    text-align: center;
+    margin-bottom: 24px;
+    background: -webkit-linear-gradient(#2ABEBE, #08A5F1);
+    background-clip: text;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
+
+  img {
+    height: 280px;
+  }
+`
+
+export const Separator = styled.hr`
+  width: 90%;
+  border-top: 1px solid #636366;
+`
+
+export const Footer = styled.div`
+  padding: 35px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  img {
+    height: 160px;
+  }
+`
+
+export const FooterSocialSection = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center; 
+  margin-top: 30px;
+
+  a {
+    text-decoration: none;
+    margin-top: 2px;
+  }
+
+  img {
+    height: 45px;
+    margin: 0 10px;
+  }
+`
